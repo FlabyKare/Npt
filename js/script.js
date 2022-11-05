1940108284df7779887b13ca97cea86d35944bb3
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-11-05 15:13:11 +0300

    a

--- js/script.js
+++ js/script.js
@@ -58,3 +58,18 @@ menuLinks.forEach((link) =>
       btn_animation();
    })
 );
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+   anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+         behavior: "smooth",
+         block: "start",
+      });
+   });
+}
