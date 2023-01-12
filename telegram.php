9fbb8669aa83dce624cd320642ae97f2a622f86e
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2023-01-12 12:33:14 +0300

    add 2 files ( thanks.html and telegram.php )

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+
+$token = "5458501936:AAFNwuGja_j5FJHR2WzsOutvSVQSlEZOo4w";
+$chat_id = "-825651915";
+$arr = array(
+  'Имя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение: ' => $text,
+);
+
+foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
+
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+if ($sendToTelegram) {
+    header('Location: pages/thanks.html');
+  } else {
+    echo "Error";
+  }
+?>
